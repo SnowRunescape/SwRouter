24cd4f565ec9dc9377781b2382107e96d95b6504
Bruno Caitano <52182057+SnowRunescape@users.noreply.github.com>
2021-07-11 12:54:20 -0400

    feat: removendo código de teste

--- app/Core/Routes.php
+++ app/Core/Routes.php
@@ -3,7 +3,4 @@ use App\Core\Router;
 
 use App\Controllers\IndexController;
 
-use App\Middlewares\AuthMiddleware;
-use App\Middlewares\StorePermissionMiddleware;
-
 Router::get("/", [IndexController::class]);
