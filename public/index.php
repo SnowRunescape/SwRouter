b519b1dfa292a7976d36cc24681d35e52c04fc64
Bruno Caitano <snowrunescape@snowdev.com.br>
2023-01-02 15:52:37 -0400

    fix: path .env

--- public/index.php
+++ public/index.php
@@ -1,16 +1,16 @@
 <?php
 
-require_once '../vendor/autoload.php';
+require_once "../vendor/autoload.php";
 
 use App\Core\Router;
 use App\Core\Database;
 use App\Controllers\BaseController;
 
-\Dotenv\Dotenv::createUnsafeImmutable(__DIR__)->safeLoad();
+\Dotenv\Dotenv::createUnsafeImmutable(__DIR__ . "/../")->safeLoad();
 
 define("APPLICATION_START", microtime(true));
 
-define("APPLICATION_PATH", __DIR__ . '/../app/');
+define("APPLICATION_PATH", __DIR__ . "/../app/");
 
 BaseController::setTemplate("templates/default");
 
